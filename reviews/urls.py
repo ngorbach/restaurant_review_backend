a2ef36737b5e9823cceec38e314c3823be7bfc95
Nico Gorbach <nico.gorbach@gmail.com>
2020-03-31 17:03:22 +0200

    list reviews endpoint

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import ListCreateReview
+from .views import ListCreateReviewView
 urlpatterns = [
-    path('', ListCreateReview()),
+    path('', ListCreateReviewView.as_view()),
 ]
-
