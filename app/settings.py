6e022d201b900695d775095a7f080c2581c5fed5
Nico Gorbach <nico.gorbach@gmail.com>
2020-03-31 20:57:54 +0000

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -167,7 +167,4 @@ AUTH_USER_MODEL = 'users.User'
 CORS_ORIGIN_ALLOW_ALL = True
 
 
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "/static-files/")
