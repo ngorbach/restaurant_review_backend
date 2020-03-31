c0de4bf57cdf4c4af83c556ab1ddc140fd14cae3
Nico Gorbach <nico.gorbach@gmail.com>
2020-03-31 19:02:21 +0000

    Update models.py

--- reviews/models.py
+++ reviews/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from datetime import datetime
 from django.conf import settings
+from restaurants.models import Restaurant
+from django.contrib.auth import get_user_model
 # Create your models here.
 
+User = get_user_model()
 
 # CREATE NEW REVIEW
 class Review(models.Model):
@@ -11,5 +14,5 @@ class Review(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='review', on_delete=models.CASCADE)
-    #restaurant = models.ForeignKey(to=settings.Restaurant, related_name='review', on_delete=models.CASCADE)
-
+    restaurant = models.ForeignKey(to=Restaurant, related_name='review', on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='review_likes', blank=True)
