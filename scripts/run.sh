1d0ec8b77e00d261e7ee5cb75fbf54427481baa8
Nico Gorbach <nico.gorbach@gmail.com>
2020-03-31 01:10:44 +0000

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python manage.py makemigrations
 python manage.py migrate
-#python manage.py collectstatic --no-input
-python manage.py collectstatic
+python manage.py collectstatic --no-input
+#python manage.py collectstatic
 gunicorn -w 4 -b 0.0.0.0:8000 app.wsgi:application
