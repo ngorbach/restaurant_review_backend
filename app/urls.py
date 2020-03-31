8a48d86f56875330bc7868dd59b1fc492224660e
Nico Gorbach <nico.gorbach@gmail.com>
2020-03-31 19:05:50 +0000

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -38,5 +38,5 @@ from django.urls import include
 urlpatterns.append(path('backend/api/categories/', include('categories.urls')))
 
 from django.urls import include
-urlpatterns.append(path('backend/api/review/comment/', include('comments.urls')))
+urlpatterns.append(path('backend/api/reviews/comment/', include('comments.urls')))
 
