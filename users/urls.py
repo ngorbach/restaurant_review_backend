a29bd0011c2be6707b9a8f581c46c40d208dd152
Joy Yoo <joyannayoo@gmail.com>
2020-03-31 15:27:32 +0200

    undid urls

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import ListCreatePostView, GetUpdateDeletePostView
 
 urlpatterns = [
-
+    path('', ListCreatePostView.as_view()),
+    path('<int:user_id>/', GetUpdateDeletePostView.as_view())
 ]
