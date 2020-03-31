3fb2e5cd35a1d2cdc3b5bced4f155e02e42d1416
Nico Gorbach <nico.gorbach@gmail.com>
2020-03-31 19:03:31 +0000

    Update models.py

--- comments/models.py
+++ comments/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from reviews.models import Review
 
 # Create your models here.
 from django.db import models
@@ -12,14 +13,17 @@ class Comment(models.Model):
         related_name='comments',
         on_delete=models.CASCADE
     )
-    review = models.ManyToManyField(to=User, related_name='review', blank=True)
+    #review = models.ManyToManyField(User, related_name='review', blank=True)
+    review = models.ForeignKey(to=Review, related_name='comments', on_delete=models.CASCADE)
+
     text_content = models.CharField(
         verbose_name='text_content',
         max_length=500,
     )
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
-    likes = models.ManyToManyField(to=User, related_name='likes', blank=True)
 
+    likes = models.ManyToManyField(User, related_name='comment_likes', blank=True)
+    
     def __str__(self):
         return  f'Item {self.text_content} by {self.user}'
