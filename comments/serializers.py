3280dab4910a727c78cdea06545e0cf88d605138
Nico Gorbach <nico.gorbach@gmail.com>
2020-04-03 08:55:48 +0200

    serializers

--- comments/serializers.py
+++ comments/serializers.py
@@ -3,6 +3,7 @@ from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Comment
         fields = '__all__'
--- users/serializers.py
+++ users/serializers.py
@@ -4,9 +4,15 @@ from rest_framework import serializers
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    num_reviews = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         exclude = ['password']
+
+    @staticmethod
+    def get_num_reviews(self):
+        return self.review.count()
 
 
 class UserNameSerializer(serializers.ModelSerializer):
